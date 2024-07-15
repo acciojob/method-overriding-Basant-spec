e4913f8aa2570ca4bde54bcee2e33f5c2b6f1de9
Basant <basantdhiman9@gmail.com>
2024-07-15 19:03:26 +0530

    newoverc

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,12 @@
 package com.driver;
 
-class A {
+public static class A {
     public static String meth(){
         return "Invoking method from class A";
     }
 }
 
-class B extends A {
+public static class B extends A {
 
     public static String meth(){
         return "Method is overridden in Extended class B";
